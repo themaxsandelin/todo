defff66bd4a1ca109fdf8fb104a1d43ffd35a7bf
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-18 19:58:58 +0100

    cleaning up code

--- resources/js/main.js
+++ resources/js/main.js
@@ -12,6 +12,9 @@ renderTodoList();
 
 // User clicked on the add button
 // If there is any text inside the item field, add that text to the todo list
+function addItemValue() {
+  
+}
 document.getElementById('add').addEventListener('click', function() {
   let value = document.getElementById('item').value;
   if (value) {
