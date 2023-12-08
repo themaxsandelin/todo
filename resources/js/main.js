5c831908b160b89f45e04b54af815671d0498371
Ibrahim Faisal <hyibrahimfaisal@gmail.com>
2023-12-09 00:54:59 +0500

    Added some noticable changes

--- resources/js/main.js
+++ resources/js/main.js
@@ -12,7 +12,7 @@ renderTodoList();
 
 // User clicked on the add button
 // If there is any text inside the item field, add that text to the todo list
-document.getElementById('add').addEventListener('click', function() {
+document.getElementById('shine').addEventListener('click', function() {
   var value = document.getElementById('item').value;
   if (value) {
     addItem(value);
